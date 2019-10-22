9d4408af68e26935a72d95d7022878b106a77e20
Peter Tran <tran.peter08@gmail.com>
2019-10-22 08:56:41 -0700

    added contiguous history

---
+++ find-contiguous-history/findContiguousHistory.js
@@ -0,0 +1,53 @@
+function findContiguousHistory(urls1 = [], urls2 = []) {
+  const histories1 = getAllHistory(urls1);
+
+  console.log(findMatches(histories1, urls2))
+  // return findMatches(histories1, urls2);
+}
+
+function getAllHistory(urls) {
+  const histories = {};
+
+  for (let i = 0; i < urls.length; i++) {
+    for (let j = i + 1; j <= urls.length; j++) {
+      const history = urls.slice(i, j).join(',');
+      if (!histories[history]) {
+
+        histories[history] = true;
+      }
+    }
+  }
+
+  return histories;
+}
+
+function findMatches(histories, urls) {
+  let max = 0;
+  let results = [];
+
+  for (let i = 0; i < urls.length; i++) {
+    for (let j = i + 1; j <= urls.length; j++) {
+      const arr = urls.slice(i, j);
+      const length = arr.length;
+      const history = arr.join(',');
+      if (histories[history] && length > max) {
+        max = length;
+        results = history.split(',');
+      }
+    }
+  }
+
+  return results;
+}
+
+const user0 = ["/start", "/pink", "/register", "/orange", "/red", "a"]
+const user1 = ["/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"]
+const user2 = ["a", "/one", "/two"]
+const user3 = ["/red", "/orange", "/yellow", "/green", "/blue", "/purple", "/white", "/amber", "/HotRodPink", "/BritishRacingGreen"]
+const user4 = ["/red", "/orange", "/amber", "/green", "/blue", "/purple", "/white", "/lavender", "/HotRodPink", "/BritishRacingGreen"]
+const user5 = ["a"]
+
+findContiguousHistory(user0, user1); // should be ['/pink', '/register', '/orange']
+findContiguousHistory(user3, user4); // should be ["/green", "/blue", "/purple", "/white"]
+findContiguousHistory(user0, user2); // should be ['a']
+findContiguousHistory(user1, user2); // should be []
